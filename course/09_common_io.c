631b440737999115269768a71efbc8ade8e51ffd
Dmitriy Dubson <d.dubson@gmail.com>
2017-02-25 12:59:03 -0500

    Added common IO commands in course

--- course/09_common_io.c
+++ course/09_common_io.c
@@ -1,18 +1,64 @@
 #include <stdio.h>
+#include <conio.h>
+
+void example_sscanf();
+void example_sprintf();
+void example_getch();
+void example_gets_puts();
 
 int main() {
+  example_sscanf();
+  example_sprintf();
+  example_getch();
+  example_gets_puts();
+  return 0;
+}
+
+void example_sscanf() {
   // sscanf() -> reads input into string
+  printf("---- SSCANF ------\n");
   char date[] = "Saturday March 25 1998";
   int day, year;
   char weekday[20], month[20];
+
+  // Dissects date by format, into variables
+  printf("%s\n", date);
   sscanf(date, "%s %s %d %d", weekday, month, &day, &year);
-  printf("%s %d, %d = %s\n", month, &day, &year, weekday );
+  printf("%s %d, %d = %s\n", month, day, year, weekday );
+  printf("-------------------\n");
+}
 
-  // getch() -> read single character
-  //char c;
-  //c = getch();
+void example_sprintf() {
+  printf("---- SPRINTF ------\n");
+  int day = 1;
+  char month[] = "January";
+  char date[20];
 
+  sprintf(date, "The date is %s %d", month, day);
+  printf("%s\n", date);
 
+  printf("-------------------\n");
+}
 
-  return 0;
+void example_getch() {
+  printf("---- GETCH ------\n");
+
+  // Single char input
+  char ch;
+  printf("\nPress any key to continue.\n");
+  ch = getch();
+  printf("\nInput was %c\n", ch);
+
+  printf("-------------------\n");
+}
+
+void example_gets_puts() {
+  puts("---- GETS AND PUTS ------\n");
+
+  char name[50];
+  puts("\nPlease enter your name: \n");
+  gets(name);
+  printf("\nYour name is %s\n", name);
+
+  puts("-------------------------\n");
 }
