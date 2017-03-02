b559250275f7cfb5e23ab84e3642ad699d32687e
pivotal <pivotal@pivotals-MacBook-Pro.local>
2017-03-02 11:22:11 -0500

    Added more pointers in C examples

--- course/13_pointers_and_memory.c
+++ course/13_pointers_and_memory.c
@@ -1,64 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void basic_pointer_structure();
+void dynamic_mem_allocation();
 void calloc_usage();
 void resizing_array();
 void passing_arrays_to_functions();
+void basic_pointer_structure_again();
 
 int* printArray();
 int* printArrayViaPointer();
 
 int main() {
-  basic_pointer_structure();
+  dynamic_mem_allocation();
+  basic_pointer_structure_again();
   calloc_usage();
   resizing_array();
   passing_arrays_to_functions();
   return 0;
 }
 
+void basic_pointer_structure_again() {
+  int x = 5;
+  int* y = &x;
+
+  printf("--------- POINTER BASICS ---------- \n");
+  printf("\nint x is: %d\n", x);
+  printf("address of int x is: %p\n", &x);
+
+  printf("pointer y contents: %p\n", y);
+  printf("pointer y points to: %d\n", *y);
+  printf("pointer y address: %p\n", &y);
+  printf("----------------------------------- \n");
+}
+
 void passing_arrays_to_functions() {
   int numbers[] = {1,2,3};
-  int* newArray = (int*) malloc(3*sizeof(int));
+  int* newArray = (int*) malloc(2*sizeof(int));
+  newArray[0] = 4;
+  newArray[1] = 5;
+
   // Pass-by-reference
+  printArrayViaPointer(newArray, 2);
+  free(newArray);
+
   newArray = printArray(numbers, 3);
   printArrayViaPointer(newArray, 3);
 }
 
 // int pointer specifies the return value is of array type
 int* printArray(int numbers[], int sizeOfArray) {
+  printf("\nPrint Array:\n");
   for(int i = 0; i < sizeOfArray; i++) {
-    printf("%d ", numbers[i]);
+    printf("[%p] %d \n", &numbers[i], numbers[i]);
   }
   printf("\n");
   return numbers;
 }
 
 int* printArrayViaPointer(int* numbers, int sizeOfArray){
+  printf("\nArray Contents via Pointer:\n");
   for(int i = 0; i < sizeOfArray; i++) {
-    printf("%d ", numbers[i]);
+    printf("[%p] %d\n", &numbers[i], numbers[i]);
   }
-  printf("\n");
+  printf("\n[%p] %d (Garbage value beyond array bound!!)\n", &numbers[4], numbers[4]);
+
   return numbers;
 }
 
-void basic_pointer_structure() {
+void dynamic_mem_allocation() {
   // Allocate memory for character pointer
   // Cast void* return from malloc to what you need -> char*
   int sizeOfCharType = sizeof(char);
+  printf("---------- Dynamic Memory Allocation ---------\n");
   printf("sizeof(char): %d byte.\n\n", sizeOfCharType);
 
   char *c = (char*) malloc(sizeOfCharType);
   *c = 'a';
 
   printf("[CONTENTS]  c: %s\n", c);
-  printf("[ADDRESS]  &c: 0x%dh\n", &c);
+  printf("[ADDRESS]  &c: %p\n", &c);
   printf("[VALUE]    *c: %d\n", *c);
   //printf("[DEREF]   **c: %s\n", **c);
 
   // Free up heap memory when done with pointer
   // - void free(void *address)
   free(c);
+
+  printf("----------------------------------------------\n");
 }
 
 void calloc_usage() {
